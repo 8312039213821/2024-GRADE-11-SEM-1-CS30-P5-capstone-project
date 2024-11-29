ce5a8380937894223303fdcd8c27037596a34f29
8312039213821 <0302415@spsd.sk.ca>
2024-11-29 12:10:24 -0600

    almost done with base gravity
    defo not all of it but a lot of the base gravity is done

--- geometry run/sketch.js
+++ geometry run/sketch.js
@@ -3,33 +3,52 @@
 // Nov 27, 2024
 
 let charSize = 100;
-let jumpStatus = false;
+let charY = 1002 - charSize; // Initial ground position
+let jumpStatus = false; // Is the player jumping?
+let jumpSpeed = 15; // Jump velocity
+let gravity = 1; // Gravity effect
+let velocityY = 0; // Vertical velocity
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
-  background(220);
-
-  drawPlayer();
+  drawGame();
   runGravity();
 }
 
-function drawPlayer(){
-  let charY = windowHeight - charSize;
+function drawGame() {
+  background(220);
+  drawPlayer();
+}
 
+function drawPlayer() {
   rectMode(CORNER);
-  fill(50, 200, 50)
-  rect(windowWidth/4, charY, charSize, charSize); //main body
+  fill(50, 200, 50);
+  rect(windowWidth / 4, charY, charSize, charSize); // Main body
 }
 
-function runGravity(){
-  if(keyIsDown(32) === true){ //checking for spacebar press
+function runGravity() {
+  // If jump is active, apply velocity
+  if (jumpStatus) {
+    charY += velocityY; // Update character's vertical position
+    velocityY += gravity; // Apply gravity to velocity
+
+    // Check if the player lands back on the ground
+    if (charY >= height - charSize) {
+      charY = height - charSize; // Reset position to ground
+      jumpStatus = false; // End jump
+      velocityY = 0; // Reset velocity
+    }
+  }
+}
+
+// Listen for key press to trigger jump
+function keyPressed() {
+  if (key === ' ' && !jumpStatus) { // Spacebar to jump
     jumpStatus = true;
-  }
-
-  if(jumpStatus === true){//making the jumping happening
-    
+    velocityY = -jumpSpeed; // Initial jump velocity
   }
 }
+
